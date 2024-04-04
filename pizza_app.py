2a97a4a18c76ffcd38edff0e433189429c3973f0
wgreunke <wgreunke@gmail.com>
2024-04-04 16:54:40 -0700

    Update pizza_app.py

--- pizza_app.py
+++ pizza_app.py
@@ -15,6 +15,27 @@ st.write("Beer Portion")
 beer_pre_tax=st.number_input('What was the amount of the beer?')
 beer_num=st.number_input('How many people drank beer?')
 
-pizza_ratio=pizza_pre_tax/beer_pre
+#Do the math
+pizza_ratio=(pizza_pre_tax+beer_pre_tax)/pizza_pre_tax
+st.write(pizza_ratio)
+beer_ratio=1-pizza_ratio
+tax=total_bill-pizza_pre_tax-beer_pre_tax
+
+pizza_tax=pizza_ratio*tax
+beer_tax=beer_ratio*tax
+
+beer_per_person=(beer_pre_tax+beer_tax)/beer_num
+st.write(beer_per_person,"Beer Portion per person")
+
+
+pizza_per_person=(pizza_pre_tax+pizza_tax)/pizza_num
+st.write(pizza_per_person,"Pizza Portion per person")
+st.write(pizza_per_person+beer_per_person,"Total for people having Pizza and Beer")
+
+st.write("Validating the numbers")
+total_validation=pizza_per_person*pizza_num+beer_per_person*beer_num
+st.write(total_validation)
+
+
 
 
