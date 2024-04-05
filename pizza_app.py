546dbb94bc03f0700d9a32aebfa5a56d29fd53b5
wgreunke <wgreunke@gmail.com>
2024-04-05 09:27:40 -0700

    Cleaned up headers

--- pizza_app.py
+++ pizza_app.py
@@ -9,10 +9,11 @@ import streamlit as st
 st.title("Pizza and Beer Calculator for Fleet 18")
 #Collect the data
 total_bill=st.number_input('What is the total bill: Pizza + Beer.  Include tax.')
+st.subheader("Pizza")
 pizza_pre_tax=st.number_input('What was the amount of the pizza wihout tax?')
 pizza_num=st.number_input('How many people ate pizza?',step=1)
 
-st.write("Beer Portion")
+st.subheader("Beer")
 beer_pre_tax=st.number_input('What was the amount of the beer?')
 beer_num=st.number_input('How many people drank beer?',step=1)
 
@@ -30,16 +31,21 @@ beer_per_person=round((beer_pre_tax+beer_tax)/(beer_num+.00001),2)
 st.write(round(beer_per_person,2),"Beer Portion per person")
 
 
+st.header("Final Numbers With Tax")
+#st.divider()
+
 pizza_per_person=round((pizza_pre_tax+pizza_tax)/(pizza_num+.00001),2)
 st.write(pizza_per_person,"Per Person for people having just pizza")
-st.write("-------------------------")
+
 pizza_and_beer_per_person=round(pizza_per_person+beer_per_person,2)
 st.write(pizza_and_beer_per_person,"Per Person for people having Pizza and Beer")
 
-st.write("Validate the numbers")
+
+st.subheader("Validate the numbers")
 total_validation=round(pizza_per_person*pizza_num+beer_per_person*beer_num,2)
 st.write("Pizza Ratio")
 st.write(round(pizza_ratio,2))
+st.write("Total of per portion X People")
 st.write(round(total_validation,2))
 
 
