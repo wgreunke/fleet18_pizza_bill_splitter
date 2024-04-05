9f87e80a8ce25e51914a9e9fca1c22e25519bcd7
wgreunke <wgreunke@gmail.com>
2024-04-05 09:19:32 -0700

    Working version

--- pizza_app.py
+++ pizza_app.py
@@ -6,36 +6,42 @@
 #inputs- number of people, total bill, pizza bill, beer bill.
 
 import streamlit as st
-
-total_bill=st.number_input('What is the total bill - Pizza + Beer.  Include tax.')
+st.title("Pizza and Beer Calculator for Fleet 18")
+#Collect the data
+total_bill=st.number_input('What is the total bill: Pizza + Beer.  Include tax.')
 pizza_pre_tax=st.number_input('What was the amount of the pizza wihout tax?')
-pizza_num=st.number_input('How many people ate pizza?')
+pizza_num=st.number_input('How many people ate pizza?',step=1)
 
 st.write("Beer Portion")
 beer_pre_tax=st.number_input('What was the amount of the beer?')
-beer_num=st.number_input('How many people drank beer?')
+beer_num=st.number_input('How many people drank beer?',step=1)
 
 #Do the math
-pizza_ratio=(pizza_pre_tax+beer_pre_tax)/pizza_pre_tax
-st.write(pizza_ratio)
+pizza_ratio=(pizza_pre_tax)/(pizza_pre_tax+beer_pre_tax+.001)
 beer_ratio=1-pizza_ratio
 tax=total_bill-pizza_pre_tax-beer_pre_tax
 
 pizza_tax=pizza_ratio*tax
 beer_tax=beer_ratio*tax
 
-beer_per_person=(beer_pre_tax+beer_tax)/beer_num
-st.write(beer_per_person,"Beer Portion per person")
+#Show the answer
+fake_button=st.button("Add it Up!")
+beer_per_person=round((beer_pre_tax+beer_tax)/(beer_num+.00001),2)
+st.write(round(beer_per_person,2),"Beer Portion per person")
 
 
-pizza_per_person=(pizza_pre_tax+pizza_tax)/pizza_num
-st.write(pizza_per_person,"Pizza Portion per person")
-st.write(pizza_per_person+beer_per_person,"Total for people having Pizza and Beer")
+pizza_per_person=round((pizza_pre_tax+pizza_tax)/(pizza_num+.00001),2)
+st.write(pizza_per_person,"Per Person for people having just pizza")
+st.write("-------------------------")
+pizza_and_beer_per_person=round(pizza_per_person+beer_per_person,2)
+st.write(pizza_and_beer_per_person,"Per Person for people having Pizza and Beer")
 
-st.write("Validating the numbers")
-total_validation=pizza_per_person*pizza_num+beer_per_person*beer_num
-st.write(total_validation)
+st.write("Validate the numbers")
+total_validation=round(pizza_per_person*pizza_num+beer_per_person*beer_num,2)
+st.write("Pizza Ratio")
+st.write(round(pizza_ratio,2))
+st.write(round(total_validation,2))
 
 
 
-
+st.write("Written by Ward Greunke")
